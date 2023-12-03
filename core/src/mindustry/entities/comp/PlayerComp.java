fbf6eaff4caeb55d7e94e8621408eb9560235fcd
nanmenyangde <nanmenyangde@foxmail.com>
2023-12-03 15:55:56 +0800

    API: add PlayerTeamChangedEvent

--- core/src/mindustry/entities/comp/PlayerComp.java
+++ core/src/mindustry/entities/comp/PlayerComp.java
@@ -23,6 +23,7 @@ import mindustry.net.Packets.*;
 import mindustry.ui.*;
 import mindustry.world.blocks.storage.*;
 import mindustry.world.blocks.storage.CoreBlock.*;
+import mindustryX.events.*;
 
 import static mindustry.Vars.*;
 
@@ -190,10 +191,13 @@ abstract class PlayerComp implements UnitController, Entityc, Syncc, Timerc, Dra
     }
 
     public void team(Team team){
+        Team last = this.team;
         this.team = team;
         if(unit != null){
             unit.team(team);
         }
+        //MDTX: PlayerTeamChangeEvent
+        Events.fire(new PlayerTeamChangedEvent(last, self()));
     }
 
     public void clearUnit(){
