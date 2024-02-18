0e17305b913ce75cab3f4baa499bde1439287650
way-zer <himc.wicp@gmail.com>
2024-02-18 17:12:05 +0800

    C(RenderExt) hiddenItemTransparency(桥梁交叉器透视)

--- core/src/mindustry/world/blocks/distribution/Junction.java
+++ core/src/mindustry/world/blocks/distribution/Junction.java
@@ -1,11 +1,18 @@
 package mindustry.world.blocks.distribution;
 
+import arc.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
 import arc.util.*;
 import arc.util.io.*;
+
+import mindustry.ui.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -78,6 +85,29 @@ public class Junction extends Block{
             Building to = nearby(relative);
             return to != null && to.team == team;
         }
+
+        @Override
+        public void draw(){
+            super.draw();
+            if(RenderExt.hiddenItemTransparency > 0){
+                float begx, begy, endx, endy;
+                for(int dir = 0; dir < 4; dir++){
+                    endx = x + Geometry.d4(dir).x * tilesize / 2f + Geometry.d4(Math.floorMod(dir + 1, 4)).x * tilesize / 4f;
+                    endy = y + Geometry.d4(dir).y * tilesize / 2f + Geometry.d4(Math.floorMod(dir + 1, 4)).y * tilesize / 4f;
+                    begx = x - Geometry.d4(dir).x * tilesize / 4f + Geometry.d4(Math.floorMod(dir + 1, 4)).x * tilesize / 4f;
+                    begy = y - Geometry.d4(dir).y * tilesize / 4f + Geometry.d4(Math.floorMod(dir + 1, 4)).y * tilesize / 4f;
+
+                    Item item;
+                    for(int i = 0; (item = buffer.getItem(dir, i)) != null; i++){
+                        float time = buffer.getTime(dir, i);
+                        float p = Math.min(((Time.time - time) * timeScale / speed), (float)(capacity - i) / capacity);
+                        Draw.alpha(RenderExt.hiddenItemTransparency / 100f);
+                        Draw.rect(item.uiIcon, Mathf.lerp(begx, endx, p), Mathf.lerp(begy, endy, p), 4f, 4f);
+                    }
+                }
+            }
+        }
+
 
         @Override
         public void write(Writes write){
