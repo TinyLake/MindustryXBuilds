91bb76afa89d83ebc0f4fc90c2d0368a13008894
way-zer <himc.wicp@gmail.com>
2024-02-18 15:57:51 +0800

    C(RenderExt) bulletShow showMineBeam displayAllMessage arcTurretPlaceCheck

--- core/src/mindustryX/features/RenderExt.java
+++ core/src/mindustryX/features/RenderExt.java
@@ -1,12 +1,20 @@
 package mindustryX.features;
 
 import arc.*;
+import arc.graphics.g2d.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.world.blocks.logic.MessageBlock.*;
 
 public class RenderExt{
+    public static boolean bulletShow, showMineBeam, displayAllMessage;
+
     public static void init(){
         Events.run(Trigger.update, () -> {
+            bulletShow = Core.settings.getBool("bulletShow");
+            showMineBeam = Core.settings.getBool("showminebeam");
+            displayAllMessage = Core.settings.getBool("displayallmessage");
         });
         Events.run(Trigger.draw, RenderExt::draw);
     }
@@ -16,6 +24,11 @@ public class RenderExt{
     }
 
     public static void onGroupDraw(Drawc t){
+        if(!bulletShow && t instanceof Bulletc) return;
+        if(!showMineBeam && t instanceof Minerc) return;
         t.draw();
+        if(displayAllMessage && t instanceof MessageBuild build){
+            Draw.draw(Layer.overlayUI - 0.1f, build::drawSelect);
+        }
     }
 }
