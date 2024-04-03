1c4fb606d7707d61c9af6f84f5ed40434675a917
way-zer <himc.wicp@gmail.com>
2024-04-03 21:57:24 +0800

    API: Hooks.beforeInit

--- core/src/mindustryX/Hooks.java
+++ core/src/mindustryX/Hooks.java
@@ -9,6 +9,12 @@ import mindustryX.features.*;
 import java.net.*;
 
 public class Hooks implements ApplicationListener{
+    /** invoke before `Vars.init`. Note that may be executed from `Vars.loadAsync` */
+    public static void beforeInit(){
+        Log.infoTag("MindustryX", "Hooks.beforeInit");
+    }
+
+    /** invoke after loading, just before `Mod::init` */
     @Override
     public void init(){
         Log.infoTag("MindustryX", "Hooks.init");
