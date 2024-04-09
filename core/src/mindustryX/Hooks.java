ce76c35a32380a536ef263d127ec9c64565c3575
way-zer <himc.wicp@gmail.com>
2024-04-09 18:23:19 +0800

    API: Hooks.onHttp

---
+++ core/src/mindustryX/Hooks.java
@@ -0,0 +1,29 @@
+package mindustryX;
+
+import arc.*;
+import arc.util.*;
+
+import java.net.*;
+
+public class Hooks implements ApplicationListener{
+    @Override
+    public void init(){
+        Log.infoTag("MindustryX", "Hooks.init");
+    }
+
+    @SuppressWarnings("unused")//call before arc.util.Http$HttpRequest.block
+    public static void onHttp(Http.HttpRequest req){
+        if(Core.settings.getBool("githubMirror")){
+            try{
+                String url = req.url;
+                String host = new URL(url).getHost();
+                if(host.contains("github.com") || host.contains("raw.githubusercontent.com")){
+                    url = "https://gh.tinylake.tech/" + url;
+                    req.url = url;
+                }
+            }catch(Exception e){
+                //ignore
+            }
+        }
+    }
+}
