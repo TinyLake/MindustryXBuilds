f148689f642ebeed848653d5b3b4bd4782e76051
way-zer <himc.wicp@gmail.com>
2024-04-25 14:49:41 +0800

    C: 根据texture排序，优化Batch
    大幅优化渲染性能
    UPDATE1: 增加配置项，默认关闭
    UPDATE2: 优化性能；我真的已经尽力了，剩下的只能怪java慢了
    UPDATE3: 完全重写了sort，又提升了一倍sort性能
    UPDATE4: 修复一些bug，并优化性能。这次重写了IntIntMap。(排序算法相当于完全重写完了)
    UPDATE5: 修复文字渲染错误

--- core/src/mindustryX/features/Settings.java
+++ core/src/mindustryX/features/Settings.java
@@ -45,6 +45,9 @@ public class Settings{
             c.addCategory("entitySettings");
             c.checkPref("bulletShow", true);
             c.checkPref("showMineBeam".toLowerCase(), true);
+
+            c.addCategory("developerMode");
+            c.checkPref("renderSort", false);
         });
     }
 }
