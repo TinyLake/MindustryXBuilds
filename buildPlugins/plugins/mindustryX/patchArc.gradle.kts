332606c27aba1c34c1ebf4d88e7cec260efae3dd
way-zer <himc.wicp@gmail.com>
2024-04-27 18:57:02 +0800

    fixup! BUILD buildPlugins patchArc改写成transform

--- buildPlugins/plugins/mindustryX/patchArc.gradle.kts
+++ buildPlugins/plugins/mindustryX/patchArc.gradle.kts
@@ -2,33 +2,25 @@ package mindustryX
 
 import javassist.ClassPool
 import javassist.CtClass
-import javassist.CtField
 import javassist.bytecode.Bytecode
 import javassist.bytecode.Descriptor
-import org.gradle.jvm.tasks.Jar
 import java.lang.reflect.Modifier
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
 
 plugins {
     java
 }
 
-tasks {
-    val patchSrc by configurations.creating
-    dependencies {
-        configurations.named("api").configure {
-            val arcLib = dependencies.find { it.name == "arc-core" }
-                ?: error("Can't find arc-core")
-            dependencies.remove(arcLib)
-            patchSrc(arcLib)
-        }
-    }
-    val patchArc by registering(Jar::class) {
-        group = "build"
-        destinationDirectory.set(temporaryDir)
-        archiveBaseName.set("patched")
-        inputs.files(patchSrc)
-        val transform = mutableMapOf<String, CtClass.() -> Unit>()
-        transform["arc.util.Http\$HttpRequest"] = clz@{
+@CacheableTransform
+abstract class PatchArc : TransformAction<TransformParameters.None> {
+    @get:InputArtifact
+    @get:PathSensitive(PathSensitivity.NONE)
+    abstract val inputArtifact: Provider<FileSystemLocation>
+
+    private val genDir = File("build/gen/patchedArc")
+    private val transform = mapOf<String, CtClass.() -> Unit>(
+        "arc.util.Http\$HttpRequest" to clz@{
             getDeclaredMethod("block").apply {
                 val code = Bytecode(methodInfo.constPool)
                 val desc = Descriptor.ofMethod(CtClass.voidType, arrayOf(this@clz))
@@ -37,32 +29,68 @@ tasks {
                 methodInfo.codeAttribute.iterator().insertEx(code.get())
                 methodInfo.rebuildStackMapIf6(classPool, classFile)
             }
-        }
-        transform["arc.graphics.g2d.DrawRequest"] = clz@{
+        },
+        "arc.graphics.g2d.DrawRequest" to clz@{
             modifiers = modifiers or Modifier.PUBLIC
 //            addField(CtField.make("public int zOffset;", this@clz))
         }
+    )
 
-        val genDir = layout.buildDirectory.dir("generated/patched")
-        from(genDir)
-        from(zipTree(patchSrc.files.single()))
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    override fun transform(outputs: TransformOutputs) {
+        val input = inputArtifact.get().asFile
+        val output = outputs.file("${input.nameWithoutExtension}.patched.jar")
+        doTransform(input, output)
+    }
 
-        doFirst {
-            genDir.get().asFileTree.forEach { it.delete() }
+    private fun doTransform(input: File, output: File) {
+        val pool = ClassPool()
+        pool.appendSystemPath()
+        pool.appendClassPath(input.path)
 
-            val pool = ClassPool()
-            pool.appendSystemPath()
-            patchSrc.files.forEach {
-                pool.appendClassPath(it.path)
-            }
+        val tmp = output.resolveSibling("tmp")
+        if (tmp.exists()) tmp.deleteRecursively()
+        tmp.mkdirs()
+        transform.forEach { (clz, block) ->
+            pool.get(clz).also(block)
+                .writeFile(tmp.path)
+        }
 
-            transform.forEach { (clz, block) ->
-                pool.get(clz).also(block)
-                    .writeFile(genDir.get().asFile.path)
+        val overwrite = tmp.walk().associateBy { it.toRelativeString(tmp).replace(File.separatorChar, '/') }
+        ZipOutputStream(output.outputStream()).use { out ->
+            ZipFile(input).use { zip ->
+                for (entry in zip.entries()) {
+                    out.putNextEntry(entry)
+                    (overwrite[entry.name]?.also {
+                        println("patchArc ${entry.name}")
+                    }?.inputStream() ?: zip.getInputStream(entry)).copyTo(out)
+                    out.closeEntry()
+                }
             }
         }
+        tmp.deleteRecursively()
     }
-    dependencies { "api"(files(patchArc)) }
-    named("compileJava") { dependsOn(patchArc) }
+}
+
+val artifactType = Attribute.of("artifactType", String::class.java)
+val patched = Attribute.of("patched", Boolean::class.javaObjectType)
+tasks {
+    dependencies {
+        attributesSchema {
+            attribute(patched)
+        }
+        artifactTypes.getByName("jar") {
+            attributes.attribute(patched, false)
+        }
+        configurations.named("api") {
+            val arcLib = dependencies.find { it.name == "arc-core" }
+                ?: error("Can't find arc-core")
+            (arcLib as ExternalModuleDependency).attributes {
+                attribute(patched, true)
+            }
+        }
+        registerTransform(PatchArc::class) {
+            from.attribute(artifactType, "jar").attribute(patched, false)
+            to.attribute(artifactType, "jar").attribute(patched, true)
+        }
+    }
 }
