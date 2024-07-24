49f5f7afb00fb572bf3f61fa1e53dcc4290edfd6
way-zer <himc.wicp@gmail.com>
2024-07-24 17:56:15 +0800

    C: Portable Data Dir

--- core/src/mindustry/ClientLauncher.java
+++ core/src/mindustry/ClientLauncher.java
@@ -36,6 +36,9 @@ public abstract class ClientLauncher extends ApplicationCore implements Platform
     @Override
     public void setup(){
         String dataDir = OS.env("MINDUSTRY_DATA_DIR");
+        if(app.isDesktop() && dataDir == null && Fi.get("data").isDirectory()){
+            dataDir = "data";
+        }
         if(dataDir != null){
             Core.settings.setDataDirectory(files.absolute(dataDir));
         }
