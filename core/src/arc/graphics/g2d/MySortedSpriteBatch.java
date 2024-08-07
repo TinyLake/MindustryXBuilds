a0a6d96cf159d1aba7d3e56e7d1d7a3d0347e96d
way-zer <himc.wicp@gmail.com>
2024-08-07 15:44:23 +0800

    fixup! OC: 优化Batch性能

--- core/src/arc/graphics/g2d/MySortedSpriteBatch.java
+++ core/src/arc/graphics/g2d/MySortedSpriteBatch.java
@@ -17,8 +17,8 @@ public class MySortedSpriteBatch extends SortedSpriteBatch2{
 
     @Override
     protected void z(float z){
-        if(this.z == z) return;
         orderZ = 0;
+        if(this.z == z) return;
         super.z(z);
     }
 
