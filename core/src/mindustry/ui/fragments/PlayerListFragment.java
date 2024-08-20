fc1b9ff1c3320be3e5c4017891bb8beaae7c037f
way-zer <himc.wicp@gmail.com>
2024-08-20 16:19:24 +0800

    fixup! ARC merged

--- core/src/mindustry/ui/fragments/PlayerListFragment.java
+++ core/src/mindustry/ui/fragments/PlayerListFragment.java
@@ -216,30 +216,7 @@ public class PlayerListFragment{
                             //there's generally no reason to team switch outside PvP or sandbox, and it's basically an easy way to cheat
                             if(allowTeamSwitch){
                                 t.button("@player.team", Icon.redo, bstyle, () -> {
-                                    var teamSelect = new BaseDialog(Core.bundle.get("player.team") + ": " + user.name);
-                                    teamSelect.setFillParent(false);
-
-                                    var group = new ButtonGroup<>();
-
-                                    int i = 0;
-
-                                    for(Team team : Team.baseTeams){
-                                        var b = new ImageButton(Tex.whiteui, Styles.clearNoneTogglei);
-                                        b.margin(4f);
-                                        b.getImageCell().grow();
-                                        b.getStyle().imageUpColor = team.color;
-                                        b.clicked(() -> {
-                                            Call.adminRequest(user, AdminAction.switchTeam, team);
-                                            teamSelect.hide();
-                                        });
-                                        teamSelect.cont.add(b).size(50f).checked(a -> user.team() == team).group(group);
-
-                                        if(i++ % 3 == 2) teamSelect.cont.row();
-                                    }
-
-                                    teamSelect.addCloseButton();
-                                    teamSelect.show();
-
+                                    UIExt.teamSelect.pickOne((it) -> Call.adminRequest(user, AdminAction.switchTeam, it), user.team());
                                     dialog.hide();
                                 }).row();
                             }
